147600274cf37e7c11ec5e2470fe7fdedaf3364a
ambersun <ambersun1019.shawn@gmail.com>
2018-05-05 17:31:17 +0800

    add statuscolumn.php

---
+++ statusColumn.php
@@ -0,0 +1,19 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php" style="font-size: 30px; font-family: serif;">message-board</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="signIn.php" style="font-size: 20px;">登入</a></li>
+                <li><a href="signUp.php" style="font-size: 20px;">註冊</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
