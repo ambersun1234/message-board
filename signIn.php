bbfa85d956fdbbf7290ccf6bd6c072a6554c06ad
ambersun <ambersun1019.shawn@gmail.com>
2018-05-06 22:56:47 +0800

    almost complete ui
    almost complete ui

--- signIn.php
+++ signIn.php
@@ -19,11 +19,17 @@
                 $username = $_POST["_username"]; // get data
                 $password = $_POST["_password"]; // get data
 
+                // check by username
                 $sql = "select * from account where username = '" . $username . "' and password = '" . $password . "'";
                 $query = mysqli_query( $con , $sql ); // check if user exists
                 $result = $query->num_rows;
 
-                if ( $result == 1 ) $message = "yes";
+                // check by eamil address
+                $sql1 = "select * from account where email = '" . $username . "' and password = '" . $password . "'";
+                $query = mysqli_query( $con , $sql ); // check if user exists
+                $result1 = $query->num_rows;
+
+                if ( $result == 1 || $result == 1 ) $message = "yes";
                 else $message = "no";
 
                 include "disconnectToDB.php";
@@ -32,14 +38,14 @@
 
         <!---------------------------------------------------------------------------------->
         <h2 style="text-align: center;">Sign in to <span style="color: red">message-board</span></h2>
-        
-        <div class="login">
+
+        <div class="login"> <!--login textbox-->
             <form action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] ); ?>" method="post">
                 Username or email address<br>
                 <input type="text" name="_username" size=15 value="<?php echo $username ?>"><br>
 
                 Password<br>
-                <input type="text" name="_password" size=15><br>
+                <input type="password" name="_password" size=15><br>
                 <input type="submit" value="Sign in">
             </form>
         </div>
