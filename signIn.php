eec29e274ea584d70030394b4f7c449da29c9229
ambersun <ambersun1019.shawn@gmail.com>
2018-05-07 13:54:24 +0800

    ui complete & add header redirction

--- signIn.php
+++ signIn.php
@@ -1,4 +1,4 @@
-<?php session_start() ?>
+<?php session_start() ?> <!--session start , always on the first line of the code-->
 
 <!DOCTYPE html>
 
@@ -6,48 +6,61 @@
     <head>
         <meta charset="utf-8">
         <title>Sign in</title>
-        <link href="custom.css" rel="stylesheet">
+        <link href="custom.css" rel="stylesheet" type="text/css">
     </head>
 
     <body style="background-color: #f9f9f9">
         <?php
             $username = "";
             $password = "";
+            $loginError = "";
 
-            if ( $_SERVER["REQUEST_METHOD"] == "POST" ) { // active when submit
+            if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["SUBMIT"] ) ) { // active when submit
+
                 include "connectToDB.php";
+
                 $username = $_POST["_username"]; // get data
                 $password = $_POST["_password"]; // get data
 
-                // check by username
                 $sql = "select * from account where username = '" . $username . "' and password = '" . $password . "'";
                 $query = mysqli_query( $con , $sql ); // check if user exists
                 $result = $query->num_rows;
 
-                // check by eamil address
-                $sql1 = "select * from account where email = '" . $username . "' and password = '" . $password . "'";
-                $query = mysqli_query( $con , $sql ); // check if user exists
-                $result1 = $query->num_rows;
-
-                if ( $result == 1 || $result == 1 ) $message = "yes";
-                else $message = "no";
+                if ( $result == 1 ) { // mark that the user logged in
+                    $_SESSION['user'] = $username;
+                    header("Location: index.php"); // redirt to index.php
+                }
+                else $loginError = "Incorrect username or password.";
 
                 include "disconnectToDB.php";
             }
          ?>
 
         <!---------------------------------------------------------------------------------->
-        <h2 style="text-align: center;">Sign in to <span style="color: red">message-board</span></h2>
+        <h2 style="text-align: center;">Sign in to <span style="color: #f74c3c">message-board</span></h2>
 
-        <div class="login"> <!--login textbox-->
-            <form action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] ); ?>" method="post">
-                Username or email address<br>
+        <div class="login_error">
+            <?php
+                if ( isset( $loginError ) ) echo $loginError;
+             ?>
+        </div>
+        <br>
+
+        <div class="login">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <span style="font-weight: bold">Username</span><br>
                 <input type="text" name="_username" size=15 value="<?php echo $username ?>"><br>
 
-                Password<br>
+                <span style="font-weight: bold">Password</span><br>
                 <input type="password" name="_password" size=15><br>
-                <input type="submit" value="Sign in">
+                <input type="submit" name="SUBMIT" value="Sign in">
             </form>
+        </div>
+        <br>
+        <div class="login_new_to">
+            <div class="login_new_to_message">
+                New to message-board? <a href="signUp.php">Create an account</a>
+            </div>
         </div>
     </body>
 </html>
