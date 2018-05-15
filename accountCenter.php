a989c021fcdb548b8fb1c0fb4c25d9e787fbfed7
ambersun <ambersun1019.shawn@gmail.com>
2018-05-15 13:26:34 +0800

    add basic code to accountCenter.php

---
+++ accountCenter.php
@@ -0,0 +1,30 @@
+<?php session_start(); ?>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Account Center</title>
+
+        <!-- custom.css -->
+        <link href="custom.css" rel="stylesheet" type="text/css">
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    <body style="background-color: #f9f9f9">
+        <?php include "statusColumn.php"; ?>
+
+        <?php
+            if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["SUBMIT_signup"] ) ) { // active when submit
+                include "connectToDB.php";
+
+                include "disconnectToDB.php";
+            }
+         ?>
+
+         <form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"]);?>">
+
+         </form>
+    </body>
+</html>
