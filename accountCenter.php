ef670cf4c39ffffcff28bce9713e071a505a59ba
ambersun <ambersun1019.shawn@gmail.com>
2018-05-19 18:06:37 +0800

    add basic picture code
    add basic picture code

--- accountCenter.php
+++ accountCenter.php
@@ -16,22 +16,36 @@
         <?php include "statusColumn.php"; ?>
 
         <?php
-            if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["SUBMIT_signup"] ) ) { // active when submit
-                include "connectToDB.php";
+            include "connectToDB.php";
 
-                include "disconnectToDB.php";
+            // get personal image
+            $sql = "select image from account where username = '" . $_SESSION['user'] . "'";
+            $query = mysqli_query( $con , $sql );
+            $row = $query->fetch_assoc();
+            $imageFilePath = $row["image"];
+            if ( $imageFilePath == "" ) $imageFilePath = "default.jpeg"; // if personal image not specify , set to default
+            // end get personal image
+
+            if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["SUBMIT_signup"] ) ) { // active when submit
+
             }
+            include "disconnectToDB.php";
          ?>
 
          <div class="accountCenter">
-             <form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"]);?>">
-                 <div class="container row">
-                     <div class="col-xs-6">
+             <div class="container">
+                <form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"]);?>">
+                     <div class="col-xs-3" style="border-color: green;border-style: solid;border-width: medium; padding: 5px 5px;">
+                         <?php echo "<img src='/images/" . $imageFilePath . "'>"?>
+                         <?php echo "<h2>" . $_SESSION['user'] . "</h2>"?>
+                         <input accept="image/jpeg" type="file" name="fileToUpload"><br>
+                         <button class="btn bth-default photo_button" type="submit" name="fileToUpload">Update</button>
                      </div>
-                     <div class="col-xs-6">
+                     <div class="col-xs-8" style="border-color: blue;border-style: solid;border-width: medium;">
+                         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
                      </div>
-                 </div>
-             </form>
+                </form>
+            </div>
          </div>
     </body>
 </html>
