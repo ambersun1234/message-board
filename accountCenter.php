6da24fa289fa21738de531ae98a81f5d4140db4a
ambersun <ambersun1019.shawn@gmail.com>
2018-05-19 19:03:25 +0800

    done basic ui
    done basic ui
    
    done basic ui

--- accountCenter.php
+++ accountCenter.php
@@ -18,13 +18,19 @@
         <?php
             include "connectToDB.php";
 
-            // get personal image
-            $sql = "select image from account where username = '" . $_SESSION['user'] . "'";
+            $username = $email = $usernameErr = $emailErr = $oldPasswordErr = $newPasswordErr = $cnewPasswordErr = "";
+
+            // get data
+            $sql = "select * from account where username = '" . $_SESSION['user'] . "'";
             $query = mysqli_query( $con , $sql );
             $row = $query->fetch_assoc();
+
             $imageFilePath = $row["image"];
             if ( $imageFilePath == "" ) $imageFilePath = "default.jpeg"; // if personal image not specify , set to default
-            // end get personal image
+
+            $username = $row["username"];
+            $email = $row["email"];
+            // end get data
 
             if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["SUBMIT_signup"] ) ) { // active when submit
 
@@ -35,14 +41,38 @@
          <div class="accountCenter">
              <div class="container">
                 <form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"]);?>">
-                     <div class="col-xs-3" style="border-color: green;border-style: solid;border-width: medium; padding: 5px 5px;">
+                     <div class="col-xs-3" style="padding: 5px 5px;">
+                         <h3 style="text-align: center;">Profile Picture</h3>
                          <?php echo "<img src='/images/" . $imageFilePath . "'>"?>
-                         <?php echo "<h2>" . $_SESSION['user'] . "</h2>"?>
                          <input accept="image/jpeg" type="file" name="fileToUpload"><br>
-                         <button class="btn bth-default photo_button" type="submit" name="fileToUpload">Update</button>
                      </div>
-                     <div class="col-xs-8" style="border-color: blue;border-style: solid;border-width: medium;">
-                         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                     <div class="col-xs-6" style="padding: 5px 5px;">
+                         <h3>Profile</h3>
+                         <hr>
+                         Username:<br>
+                         <input type="text" name="_username" placeholder="Pick a username" value="<?php echo $username; ?>"><br>
+                         <?php echo "<div class='signUPvalid'>" . $usernameErr . "</div>"; ?>
+
+                         Email:<br>
+                         <input type="text" name="_email" placeholder="you@gmail.com" value="<?php echo $email; ?>"><br>
+                         <?php echo "<div class='signUPvalid'>" . $emailErr . "</div>"; ?>
+
+                         <h3>Change Password</h3>
+                         <hr>
+                         Old password:
+                         <input type="password" placeholder="your old password" name="_oldPassword"><br>
+                         <?php echo "<div class='signUPvalid'>" . $oldPasswordErr . "</div>"; ?>
+
+                         New password:
+                         <input type="password" placeholder="your new password" name="_newPassword"><br>
+                         <?php echo "<div class='signUPvalid'>" . $newPasswordErr . "</div>"; ?>
+
+                         Confirm new password:
+                         <input type="password" placeholder="confirm your new password" name="_cnewPassword"><br>
+                         <?php echo "<div class='signUPvalid'>" . $cnewPasswordErr . "</div>"; ?>
+
+
+                         <button class="btn bth-default update_button" type="submit" name="fileToUpload">Update</button>
                      </div>
                 </form>
             </div>
