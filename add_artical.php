f56cab0b12554bd88e7dcbc5e55005cf6f79ffa6
ambersun <ambersun1019.shawn@gmail.com>
2018-05-27 22:18:46 +0800

    finish add_artical.php

--- add_artical.php
+++ add_artical.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <html lang="en">
 <html>
     <head>
@@ -16,18 +18,105 @@
     </head>
 
     <body  style="background-color: #f9f9f9;">
+        <?php if ( $_SESSION["loggedin"] == false ) header("Location: index.php"); ?>
+
+        <?php
+            $title = $article = $boardid = "";
+            $titleErr = $articleErr = $postErr = "";
+
+            if ( $_SERVER["REQUEST_METHOD"] == "POST" && isset( $_POST["SUBMIT"] ) ) {
+                include "connectToDB.php";
+
+                $title = getData( $con , $_POST["_title"] );
+                $article = getData( $con , $_POST["_artical"] );
+                $boardid = getData( $con , $_POST["_boardid"] );
+
+                checkTitle( $title );
+                checkArticle( $article );
+
+                if ( checkValid( $titleErr , $articleErr ) ) {
+
+                    $id = findUserid( $con , $_SESSION["user"] );
+                    $postid = findPostid( $con );
+
+                    if ( checkBoard( $boardid ) != false && $postErr == "" ) {
+                        $sql = "insert into post( userid , postid , title , article , boardid )";
+                        $sql .= " value( " . $id . " , " . $postid . " , '" . $title . "' , '" . $article . "' , '" . $boardid . "' )";
+
+                        $query = mysqli_query( $con , $sql );
+
+                        if ( !$query ) $postErr = "Something went wrong , please try again...<br>";
+                        else header("Location: Gaming.php");
+                    }
+                }
+
+                include "disconnectToDB.php";
+            }
+            function checkBoard( $board ) {
+                if ( $board == "gaming" || $board == "news" || $board == "gossip" ) return true;
+                else {
+                    $GLOBALS["postErr"] = "Something went wrong , please try again...<br>";
+                    return false;
+                }
+            }
+            function findUserid( $con , $username ) {
+                $sql = "select userid from account where username = '" . $username . "'";
+                $query = mysqli_query( $con , $sql );
+                if ( !$query ) $GLOBALS["postErr"] = "Something went wrong , please try again...<br>";
+                $row = $query->fetch_assoc();
+                return $row["userid"];
+            }
+            function findPostid( $con ) {
+                $sql = "select max( postid ) as postid from post";
+                $query = mysqli_query( $con , $sql );
+                if ( !$query ) $GLOBALS["postErr"] = "Something went wrong , please try again...<br>";
+                $row = $query->fetch_assoc();
+                if ( $row["postid"] == null ) return 0;
+                else return $row["postid"] + 1;
+            }
+            function getData( $con , $data ) {
+                $data = stripslashes( $data ); // remove all \
+                $data = htmlspecialchars( $data ); // turn &"'<> to real entity
+                $data = mysqli_real_escape_string( $con , $data );
+                return $data;
+            }
+            function checkTitle( $title ) {
+                if ( $title == "" ) $GLOBALS["titleErr"] = "Title can not be blank!!<br>";
+                else $GLOBALS["titleErr"] = "";
+            }
+            function checkArticle( $article ) {
+                if ( $article == "" ) $GLOBALS["articleErr"] = "Article can not be blank!!<br>";
+                else if ( strlen( $article ) < 10 ) $GLOBALS["articleErr"] = "Article must have at least 30 words!!<br>";
+                else $GLOBALS["articleErr"] = "";
+            }
+            function checkValid( $titleErr , $articleErr ) {
+                if ( $titleErr == "" && $articleErr == "" ) return true;
+                else return false;
+            }
+         ?>
+
         <?php include "statusColumn.php"; ?>
 
         <div class="add_artical">
-            <form name="reg" method="post">
+            <form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] );?>">
                 <br>title:<br>
-                <textarea name="title" rows="1" cols="1" maxlength="30"></textarea><br>
+                <!-- note: textarea does not have value attribute , so php echo should write in the middle of textarea -->
+                <textarea name="_title" rows="1" cols="1" maxlength="30"><?php echo $title; ?></textarea><br>
+                <?php echo "<div class='invalid'>" . $titleErr . "</div>"; ?>
 
+                <br>
                 article:<br>
-                <textarea name="artical" style="color:black;"></textarea><br><br>
+                ( article must have at least 10 words )<br>
+                <textarea name="_artical"><?php echo $article;?></textarea><br>
+                <?php echo "<div class='invalid'>" . $articleErr . "</div>"; ?>
 
-                <button type="submit" class="btn btn-default">Submit</button>
-                <button type="reset" class="btn btn-default" onClick="reset()">Reset</button>
+                <!-- pass var1 to php after submit form -->
+                <input type="hidden" name="_boardid" value="<?php echo $_GET['var1'];?>">
+
+                <br>
+                <button type="submit" name="SUBMIT" class="btn btn-default">Submit</button>
+                <button type="reset" class="btn btn-default">Reset</button>
+                <?php echo "<div class='invalid'>" . $postErr . "</div>"; ?>
             </form>
         </div>
     </body>
