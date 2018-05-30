a6e2a6047b91aa9f148708f482163683623da99c
ambersun <ambersun1019.shawn@gmail.com>
2018-05-31 01:39:33 +0800

    add navbar recognition

--- statusColumn.php
+++ statusColumn.php
@@ -1,3 +1,13 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        var url = window.location;
+        $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+        $('ul.nav a').filter(function() {
+             return this.href == url;
+        }).parent().addClass('active');
+    });
+</script>
+
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="container tool_bar">
         <div class="navbar-header">
