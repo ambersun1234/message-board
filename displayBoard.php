e5671a9cec0a0ed7c28c1fcbc814690afb7c2e95
ambersun <ambersun1019.shawn@gmail.com>
2018-05-31 00:01:54 +0800

    undo previous & next page feature

--- displayBoard.php
+++ displayBoard.php
@@ -38,7 +38,7 @@
                             echo "</span></p>";
                         echo "</div>";
                     }
-                    
+
                 }
                 else echo "There is no post yet!!<br>";
 
@@ -54,14 +54,11 @@
                     $data = mysqli_real_escape_string( $con , $data );
                     return $data;
                 }
-                echo '<button type="button" class="btn btn-primary btn-xs" style="color:black; background-color:white; position:relative; top:10px; font-size:20px;"><img src= "back.png">back</button>'.'&nbsp&nbsp&nbsp&nbsp&nbsp';
-                echo "<span style='float: right;'>";
-                echo '<button type="button" class="btn btn-primary btn-xs" style=" color:black; background-color:white; position:relative; top:10px;font-size:20px;">next <img src= "next.png"></button>';
              ?>
         </div>
         <div class="col-xs-3">
             <br>
-            <button type="button" class="btn btn-default" onclick="location.href='add_artical.php?var1=<?php echo $boardid; ?>'" style="background-color: #ff7474; color: black; font-size:5pt; position:relative; top:10px;">New post<img src="edit.png"></button>
+            <button type="button" class="btn btn-default" onclick="location.href='add_artical.php?var1=<?php echo $boardid; ?>'" style="background-color: #ff7474; color: black; font-size:15px; position:relative; top:10px;">New post<img src="edit.png"></button>
         </div>
 
     </body>
