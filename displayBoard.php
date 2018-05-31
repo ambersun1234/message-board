a50b61526c7f855fbc3cb2c4c94650a22a1ad0d5
ambersun <ambersun1019.shawn@gmail.com>
2018-05-31 15:44:20 +0800

    dynamic show new post button( by session )

--- displayBoard.php
+++ displayBoard.php
@@ -56,10 +56,11 @@
                 }
              ?>
         </div>
-        <div class="col-xs-3">
-            <br>
-            <button type="button" class="btn btn-default" onclick="location.href='add_artical.php?boardid=<?php echo $boardid; ?>'" style="background-color: #ff7474; color: black; font-size:15px; position:relative; top:10px;">New post<img src="/images/edit.png"></button>
-        </div>
-
+        <?php if ( $_SESSION["loggedin"] == true ) { ?>
+            <div class="col-xs-3">
+                <br>
+                <button type="button" class="btn btn-default" onclick="location.href='add_artical.php?boardid=<?php echo $boardid; ?>'" style="background-color: #ff7474; color: black; font-size:15px; position:relative; top:10px;">New post<img src="/images/edit.png"></button>
+            </div>
+        <?php } ?>
     </body>
 </html>
